1d50fd241415e0f4dc5f6cbed441d8b171e38cb2
Adam Adnan <110590770+AdamAdnan@users.noreply.github.com>
2026-04-25 23:20:15 -0700

    Update credit_scoring_app.py

--- credit_scoring_app.py
+++ credit_scoring_app.py
@@ -1,3 +1,5 @@
+pip install numpy pandas scipy matplotlib seaborn jupyter sqlalchemy scikit-learn==1.2.2 joblib==1.3.1 streamlit==1.24.0
+
 import streamlit as st
 import pandas as pd
 import joblib
